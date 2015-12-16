b98f3a2cdd56bf3e1cb6b933d949d3d36e8eda1c
elfas89 <elfas89@gmail.com>
2015-12-16 16:42:53 +0200

    HW3_16122015

---
+++ Homework3/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework3
+{
+    class Employee : Person
+    {
+        private int empNumber;
+        private string empPosition;
+
+        public int EmpNumber
+        {
+            get
+            {
+                return empNumber;
+            }
+        }
+
+        public string EmpPosition
+        {
+            get
+            {
+                return empPosition;
+            }
+            set
+            {
+                empPosition = value;
+            }
+        }
+
+
+        public Employee(string name, string position, int number)
+            : base(name)
+        {
+            Name = name;
+            empPosition = position;
+            empNumber = number;
+        }
+
+        public override string ToString()
+        {
+            return "Работник " + Name + ", должность " + empPosition + ", номер в базе " + empNumber;
+        }
+        
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"D:\!_C#\!_Projects\Homework3\Homework3\bin\Debug\Workers.dat";
+            //FileInfo fileInfo = new FileInfo(path);
+
+
+            List<Employee> WorkersList = new List<Employee>();
+            WorkersList.Add(new Employee("John", "Lead Victim", 0001));
+            WorkersList.Add(new Employee("Steve", "Tech Victim", 2));
+
+            //foreach (Employee e in WorkersList)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //}
+
+
+
+
+            while (true)
+            {
+                //Console.Clear();
+                //foreach (var w in WorkersList)
+                //{
+                //    Console.WriteLine(w.ToString());
+
+                //}
+                //Console.WriteLine();
+                Console.Write("Введите команду: ");
+
+                string[] commands = Console.ReadLine().Split(' ');
+
+                if (commands[0].ToLower() == "exit" & commands.Length == 1)
+                {
+                    //запись в файл
+                    return;
+                }
+
+
+
+                if (commands[0].ToLower() == "add" && commands.Length == 4)
+                {
+                    int number;
+                    if (Int32.TryParse(commands[3], out number))
+                    {
+                        WorkersList.Add(new Employee(commands[1], commands[2], number));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный номер работника!");
+                        Help();
+                    }
+
+                }
+
+
+                if (commands[0].ToLower() == "del" && commands.Length == 3)
+                {
+                    int number;
+                    if (Int32.TryParse(commands[2], out number))
+                    {
+                        
+                        IEnumerable<int> res = 
+                           from t in WorkersList // Определяем каждый объект как t
+                           where t.Name == commands[1] // Фильтрация по критерию
+                           orderby t // Упорядочиваем по возрастанию
+                           select WorkersList.IndexOf(t); // Выбираем объект
+
+                        foreach (int i in res)
+                        {
+                            WorkersList.RemoveAt(i);
+                            Console.WriteLine(i);
+                        }
+
+                        Console.WriteLine("удаление работника номер: " + number);
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный номер работника!");
+                        Help();
+                    }
+
+                }
+
+
+                //if (commands[0].ToLower() == "info" && commands.Length == 3)
+
+                if (commands[0].ToLower() == "list")
+                {
+                    foreach (var w in WorkersList)
+                    {
+                        Console.WriteLine(w.ToString());
+
+                    }
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+        static void Help()
+        {
+            Console.WriteLine("Какой-то хелп");
+            Console.WriteLine("Нажмите любую клавишу для продолжения");
+            Console.ReadLine();
+        }
+
+
+    }
+}
